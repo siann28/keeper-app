7dee113d6d76aa9165facbfb46523d6c2128eb37
simeon <simeontolentinojr@yahoo.com>
2020-06-07 23:23:04 +0800

    Keeper Part 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
-    return (
+  const [notes, setNotes] = useState([]);
+  function addNote(note) {
+    setNotes(prevNotes => {
+      return [...prevNotes, note];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
     <div>
-        <Header />
-        {notes.map(note => (
-            <Note
-            key={note.key}
-            title={note.title}
-            content={note.content} />
-        ))}
-        <Footer />
+      <Header />
+      <CreateArea 
+        onAdd={addNote}
+      />
+      {notes.map((noteItem, index) => {
+        return <Note key={index} id={index} title={noteItem.title} content={noteItem.content} onClick={deleteNote} />
+      })}
+      
+      <Footer />
     </div>
-    );
+  );
 }
 
 export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -5,6 +5,10 @@ function Note(props) {
         <div className="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
+            <button 
+            onClick={()=> {
+            props.onClick(props.id)
+            }}>DELETE</button>
         </div>
         );
 }
